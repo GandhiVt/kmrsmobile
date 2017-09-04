530daa1320c09926c5db20041d85590c1ba871ae
GandhiVt <gandhi.vera@gmail.com>
2017-09-04 01:56:00 -0500

    updated

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={		
+	'ApiUrl':"http://www.patraer.com/mobileapp/api",				
+	'DialogDefaultTitle':"Delivery de Cerveza",	
+	'pushNotificationSenderid':"409670212325",	
+	'facebookAppId':"101091337135047", 
+	'APIHasKey':"AIzaSyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
+};
